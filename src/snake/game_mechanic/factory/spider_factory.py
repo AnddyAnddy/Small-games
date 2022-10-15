d9d4000bf4ba90202c5ade4ee254c97d80d70f04
AnddyAnddy <anddyhbc@gmail.com>
2022-10-16 00:40:26 +0200

    Add spider factory

---
+++ src/snake/game_mechanic/factory/spider_factory.py
@@ -0,0 +1,14 @@
+import random
+
+from src.snake.game_mechanic.coordinates import Position, Coordinates
+from src.snake.game_mechanic.items.spider import Spider
+
+
+class SpiderFactory:
+    def __init__(self):
+        self.count = 0
+
+    def create(self, available_positions: list[Coordinates] = None, forced_position: Position | None = None) -> Spider:
+        position = forced_position if forced_position else random.choice(available_positions)
+        self.count += 1
+        return Spider(position, f"spider_{self.count}", seconds_alive=5)
