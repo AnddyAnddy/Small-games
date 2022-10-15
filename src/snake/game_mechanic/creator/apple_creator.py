7f4b58de5799e1cfb1e4dda2592134702a4db392
AnddyAnddy <anddyhbc@gmail.com>
2022-10-15 13:23:41 +0200

    Apple creator creating Apple

---
+++ src/snake/game_mechanic/creator/apple_creator.py
@@ -0,0 +1,14 @@
+import random
+
+from src.snake.game_mechanic.coordinates import Position, Coordinates
+from src.snake.game_mechanic.items.apple import Apple
+
+
+class AppleCreator:
+    def __init__(self):
+        self.count = 0
+
+    def create(self, available_positions: list[Coordinates] = None, forced_position: Position | None = None) -> Apple:
+        position = forced_position if forced_position else random.choice(available_positions)
+        self.count += 1
+        return Apple(position, f"apple_{self.count}")
