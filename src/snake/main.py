77417e5e2bde63dae048b1973ef3bf2b14b10e76
AnddyAnddy <anddyhbc@gmail.com>
2022-10-16 00:40:16 +0200

    Attaching SpiderObserver in game

--- src/snake/main.py
+++ src/snake/main.py
@@ -1,5 +1,6 @@
 from src.snake.game_mechanic.game import Game
 from src.snake.game_mechanic.observers.log_observer import LogObserver
+from src.snake.game_mechanic.observers.spider_observer import SpiderObserver
 from src.snake.game_mechanic.observers.ui_observer import UIObserver
 from src.snake.level import load_level
 from src.snake.user_interface.drawers.factory.graphical_factory import GraphicalFactory
@@ -11,6 +12,7 @@ def graphical():
     game = Game(Graphical)
     game.attach(UIObserver())
     game.attach(LogObserver())
+    game.attach(SpiderObserver())
     load_level(game, GraphicalFactory(), "level1.txt")
     upemtk.cree_fenetre(400, 400)
     game.main_loop()
