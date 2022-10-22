a0fd2933530edc09b934d1edebdce13841348af8
AnddyAnddy <anddyhbc@gmail.com>
2022-10-22 21:25:13 +0200

    Add call to InfiniteSnakeDrawerGraphical

--- src/snake/user_interface/drawers/factory/graphical_factory.py
+++ src/snake/user_interface/drawers/factory/graphical_factory.py
@@ -2,6 +2,7 @@ from src.snake.game_mechanic.game import Game
 from src.snake.game_mechanic.items.item_type import ItemType
 from src.snake.user_interface.drawers.factory.factory_drawer import DrawerFactory
 from src.snake.user_interface.drawers.graphical.apple_drawer import AppleDrawerGraphical
+from src.snake.user_interface.drawers.graphical.infinite_snake_drawer import InfiniteSnakeDrawerGraphical
 from src.snake.user_interface.drawers.graphical.snake_drawer import SnakeDrawerGraphical
 from src.snake.user_interface.drawers.graphical.spider_drawer import SpiderDrawerGraphical
 from src.snake.user_interface.drawers.graphical.wall_drawer import WallDrawerGraphical
@@ -16,6 +17,9 @@ class GraphicalFactory(DrawerFactory):
 
     def snake_drawer(self, game: Game):
         self.add(game, ItemType.SNAKE, SnakeDrawerGraphical(game.ui))
+
+    def infinite_snake_drawer(self, game: Game):
+        self.add(game, ItemType.SNAKE, InfiniteSnakeDrawerGraphical(game.ui))
 
     def spider_drawer(self, game: Game):
         self.add(game, ItemType.SPIDER, SpiderDrawerGraphical(game.ui))
