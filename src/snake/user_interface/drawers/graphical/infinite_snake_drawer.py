ca3ae81c3435e338d6a20442d0a0b26329b1c1fd
AnddyAnddy <anddyhbc@gmail.com>
2022-10-22 21:30:20 +0200

    Optimise Infinite snake drawer

--- src/snake/user_interface/drawers/graphical/infinite_snake_drawer.py
+++ src/snake/user_interface/drawers/graphical/infinite_snake_drawer.py
@@ -7,7 +7,7 @@ from src.tictactoe.user_interface.ui_abstract import UI
 class InfiniteSnakeDrawerGraphical(Drawer):
     def __init__(self, ui: UI):
         super().__init__(ui)
-        self.optimized_drawer = InfiniteSnakeDrawerGraphical.OptimizedSnakeDrawer(self)
+        self.first_draw = False
 
     def draw_head(self, tag, x: int, y: int):
         size = self.ui.case_size
@@ -22,38 +22,10 @@ class InfiniteSnakeDrawerGraphical(Drawer):
         upemtk.rectangle(ax * size, ay * size, bx * size, by * size, remplissage='green', tag=tag)
 
     def draw(self, snake: Snake):
-        self.optimized_drawer.draw(snake)
-
-    class OptimizedSnakeDrawer:
-        def __init__(self, snake_drawer: "InfiniteSnakeDrawerGraphical"):
-            self.snake_drawer = snake_drawer
-            self.first_call = False
-            self.cursor = None
-            self.snake_size = 0
-
-        def draw(self, snake: Snake):
-            if not self.first_call:
-                self._draw_full_snake(snake, False)
-                self.first_call = True
-                return
-            # detect a change
-            if len(snake.body) != self.snake_size:
-                self._draw_full_snake(snake, True)
-                # erase tail, draw new head and update cursor
-            else:
-                upemtk.efface(f"{snake.tag}_{self.cursor}")
-                x, y = snake.body[0]
-                self.snake_drawer.draw_body(f"{snake.tag}_{self.cursor}", x, y)
-                self._update_runner(snake)
-
-        def _draw_full_snake(self, snake, erase: bool):
-            self.snake_size = len(snake.body)
+        if not self.first_draw:
             for i, (x, y) in enumerate(snake.body):
-                i_tag = f"{snake.tag}_{i}"
-                if erase:
-                    upemtk.efface(i_tag)
-                self.snake_drawer.draw_body(i_tag, x, y)
-            self.cursor = self.snake_size - 1
-
-        def _update_runner(self, snake):
-            self.cursor = self.cursor - 1 if self.cursor != 0 else len(snake.body) - 1
+                self.draw_body(snake.tag, x, y)
+            self.first_draw = True
+        else:
+            x, y = snake.body[0]
+            self.draw_body(snake.tag, x, y)
